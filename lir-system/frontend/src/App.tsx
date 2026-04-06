91e6b854f7e08b84ede738e2b9c3822aa20979f9
Wanji <ewilliammiguel@gmail.com>
2026-04-06 20:38:12 +0800

    feat(login): implement initial login screen and account type cards

--- lir-system/frontend/src/App.tsx
+++ lir-system/frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
-import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Routes>
+      {/*
       <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
       </Route>
+      */}
+      <Route path="/" element={<Login />} />
     </Routes>
   )
 }
